d2c91b546f4c75f603f5995bf8ea49755bc46550
Eric Yao <emyao@usc.edu>
2024-03-03 17:19:16 -0800

    Finished Implementation

--- defs.h
+++ defs.h
@@ -116,6 +116,7 @@ void            scheduler(void) __attribute__((noreturn));
 void            sched(void);
 void            setproc(struct proc*);
 void            sleep(void*, struct spinlock*);
+void            sleep1(void*, struct spinlock*);
 void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
--- user.h
+++ user.h
@@ -1,5 +1,7 @@
 struct stat;
 struct rtcdate;
+struct condvar;
+struct spinlock;
 
 // system calls
 int fork(void);
@@ -37,3 +39,9 @@ void* memset(void*, int, uint);
 void* malloc(uint);
 void free(void*);
 int atoi(const char*);
+
+void lock(struct spinlock *);
+void unlock(struct spinlock *);
+void init_lock(struct spinlock *);
+int cv_wait(struct condvar *);
+int cv_signal(struct condvar *);
